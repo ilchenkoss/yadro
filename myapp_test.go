e5c9d3bdfdc59dd80792584bf173a71e30408392
iclhenkoss <ilchenko_ss@mail.ru>
2024-03-28 12:49:40 +0300

    auto test begin

--- myapp_test.go
+++ myapp_test.go
@@ -23,3 +23,25 @@ func TestExample2(t *testing.T) {
 		t.Errorf("Result was incorrect, got: %s, want: %s.", actual, expected)
 	}
 }
+
+//
+//func TestGenerateWords(t *testing.T) {
+//
+//	words := 10
+//	//punctuation := map[string]bool{
+//	//	".": true,
+//	//	",": true,
+//	//	"-": true,
+//	//	"?": true,
+//	//	"1": true,
+//	//}
+//
+//	babbler := babble.NewBabbler()
+//	babbler.Separator = " "
+//	babbler.Count = words
+//	fakeString := babbler.Babble()
+//	fmt.Println(fakeString)
+//	fmt.Println(stringNormalization(fakeString))
+//
+//	return
+//}
