1664e70e2b3a2216eb886ad4e2742e9f2d825b12
ilsergey <ilchenko_ss@mail.ru>
2024-04-21 20:33:05 +0300

    change logic for create/read index.json.

--- pkg/xkcd/xkcd.go
+++ pkg/xkcd/xkcd.go
@@ -2,6 +2,8 @@ package xkcd
 
 import (
 	"context"
+	"encoding/json"
+	"myapp/pkg/database"
 	"myapp/pkg/indexing"
 	"myapp/pkg/scraper"
 )
@@ -37,9 +39,21 @@ func Xkcd(args OutputStruct) {
 		args.ScrapeCtx,
 		args.ScrapeCtxCancel)
 
-	indexDB := indexing.CreateIndexingDB(scrapeData, args.IndexPath)
+	var indexDB map[string][]int
+
+	if scrapeScore > 0 {
+		indexDB = indexing.CreateIndexingDB(scrapeData, args.IndexPath)
+	} else {
+
+		indexDBBytes := database.ReadBytesFromFile(args.IndexPath)
+		err := json.Unmarshal(indexDBBytes, &indexDB)
+
+		if err != nil || indexDBBytes == nil {
+			indexDB = indexing.CreateIndexingDB(scrapeData, args.IndexPath)
+		}
+	}
 
 	if len(args.StringRequest) > 0 {
-		indexing.ReturnComics(args.StringRequest, indexDB, scrapeData)
+		indexing.FindComics(args.StringRequest, indexDB, scrapeData)
 	}
 }
