a076739f7676119768ddc02b919a74c1042787e1
ilsergey <ilchenko_ss@mail.ru>
2024-05-07 04:34:16 +0300

    add migrations

---
+++ internal/storage/migrations/002_create_words_table.up.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS words (
+    id INTEGER PRIMARY KEY,
+    word TEXT UNIQUE
+);
---
+++ internal/storage/migrations/004_create_weights_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS weights (
+    word_id TEXT,
+    comic_id INTEGER,
+    position_id INTEGER,
+    weight REAL,
+    FOREIGN KEY (comic_id) REFERENCES comics(id),
+    FOREIGN KEY (word_id) REFERENCES words(id),
+    FOREIGN KEY (position_id) REFERENCES  positions(id)
+    );
