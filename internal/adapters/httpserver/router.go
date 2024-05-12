b243542f5d01327a3c13d0e6f5c1db47d49a1adf
ilsergey <ilchenko_ss@mail.ru>
2024-05-12 13:00:34 +0300

    add auth to router

--- internal/adapters/httpserver/router.go
+++ internal/adapters/httpserver/router.go
@@ -2,19 +2,24 @@ package httpserver
 
 import (
 	"myapp/internal/adapters/httpserver/handlers"
+	"myapp/internal/core/port"
 	"net/http"
 )
 
 type Handlers struct {
+	TokenService  port.TokenService
+	UserRepo      port.UserRepository
 	ScrapeHandler *handlers.ScrapeHandler
 	SearchHandler *handlers.SearchHandler
+	AuthHandler   *handlers.AuthHandler
 }
 
 func NewRouter(router *Handlers) *http.ServeMux {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("POST /update", router.ScrapeHandler.Update)
-	mux.HandleFunc("GET /pics", router.SearchHandler.Search)
+	mux.HandleFunc("POST /update", handlers.AdminMiddleware(router.ScrapeHandler.Update, router.TokenService, router.UserRepo))
+	mux.HandleFunc("GET /pics", handlers.OrdinaryMiddleware(router.SearchHandler.Search, router.TokenService, router.UserRepo))
+	mux.HandleFunc("POST /login", router.AuthHandler.Login)
 
 	return mux
 }
