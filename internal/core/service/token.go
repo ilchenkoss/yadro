7b022b5ed38debbc5f64ee01697dcd0267d05981
ilsergey <ilchenko_ss@mail.ru>
2024-05-12 12:58:55 +0300

    add token service

---
+++ internal/core/service/token.go
@@ -0,0 +1,67 @@
+package service
+
+import (
+	"errors"
+	"github.com/golang-jwt/jwt/v5"
+	"myapp/internal/config"
+	"myapp/internal/core/domain"
+	"time"
+)
+
+type TokenService struct {
+	SecretKey []byte
+	Duration  time.Duration
+	Method    jwt.SigningMethod
+}
+
+func NewTokenService(cfg config.HttpServerConfig) *TokenService {
+
+	duration := time.Duration(cfg.TokenMaxTime) * time.Minute
+	//load secret key
+	secretKey := []byte("SuperSecretKey")
+	method := jwt.SigningMethodHS256
+
+	return &TokenService{
+		secretKey,
+		duration,
+		method,
+	}
+}
+
+func (ts *TokenService) CreateToken(user *domain.User) (string, error) {
+
+	token := jwt.NewWithClaims(ts.Method, jwt.RegisteredClaims{
+		Subject: user.Login,
+		ExpiresAt: &jwt.NumericDate{
+			time.Now().Add(ts.Duration),
+		},
+	})
+
+	tokenString, err := token.SignedString(ts.SecretKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+
+}
+
+func (ts *TokenService) GetUserByToken(token *jwt.Token) (string, error) {
+	return token.Claims.GetSubject()
+}
+
+func (ts *TokenService) GetTokenByString(tokenString string) (*jwt.Token, error) {
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return ts.SecretKey, nil
+	})
+
+	switch {
+	case token.Valid:
+		return token, nil
+	case errors.Is(err, jwt.ErrTokenExpired):
+		return nil, domain.ErrTokenExpired
+	}
+
+	return token, err
+}
